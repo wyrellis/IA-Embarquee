5a1d4f29d8cda78758f5365571d795686f392067
berarale <berard.alexandre2@gmail.com>
2020-05-07 01:47:08 +0200

    Removed duplicata directories and cleaned up everything

--- STM32 projet determination/determination/Debug/X-CUBE-AI/App/pb_common.d
+++
@@ -1,6 +0,0 @@
-X-CUBE-AI/App/pb_common.o: ../X-CUBE-AI/App/pb_common.c \
- ../X-CUBE-AI/App/pb_common.h ../X-CUBE-AI/App/pb.h
-
-../X-CUBE-AI/App/pb_common.h:
-
-../X-CUBE-AI/App/pb.h:
--- determination - Copie/Debug/X-CUBE-AI/App/pb_encode.d
+++
@@ -1,9 +0,0 @@
-X-CUBE-AI/App/pb_encode.o: ../X-CUBE-AI/App/pb_encode.c \
- ../X-CUBE-AI/App/pb.h ../X-CUBE-AI/App/pb_encode.h \
- ../X-CUBE-AI/App/pb_common.h
-
-../X-CUBE-AI/App/pb.h:
-
-../X-CUBE-AI/App/pb_encode.h:
-
-../X-CUBE-AI/App/pb_common.h:
